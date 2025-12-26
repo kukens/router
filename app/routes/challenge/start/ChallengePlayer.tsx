debe59bf995c634a60843e46619f850d9200abce
Łukasz Zięba <kukens@wp.pl>
2025-12-26 23:19:57 +0100

    add swipe

--- app/routes/challenge/start/ChallengePlayer.tsx
+++ app/routes/challenge/start/ChallengePlayer.tsx
@@ -6,9 +6,22 @@ import { difficultyLevels, instruments } from '../Challenge';
 import { ACCORDION_CHORDS } from '~/data/accordionChords';
 import styles from '~/components/Bars.module.css';
 import type { ChordProgression } from '~/data/accordionChords';
+import { useSwipeable } from 'react-swipeable';
+
 interface ChallengeProps {
     difficulty: string,
     instrument: string
+}
+
+function shuffleArray<T>(array: T[]): T[] {
+    const shuffled = [...array];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    return shuffled;
 }
 
 export default function challengePlayer(props: ChallengeProps) {
@@ -16,31 +29,51 @@ export default function challengePlayer(props: ChallengeProps) {
     const { evaluatedChord } = useChord();
     const evaluatedChordRef = useRef(evaluatedChord);
     const evaluatedChordVerionsRef = useRef(evaluatedChord?.version);
-    const [challenge, setChallenge] = useState<ChordProgression | null>(null)
+    const [progression, setProgression] = useState<ChordProgression | null>(null)
+    const [progressionIndex, setProgressionIndex] = useState<number>(0)
+
     const tempo = 80;
 
     const beatsElementsRef = useRef<HTMLDivElement[]>([]);
     const barsElementsRef = useRef<HTMLDivElement[]>([]);
-
+    const progressions = useRef<ChordProgression[]>([]);
     const indexRef = useRef(0);
+
+    const handlers = useSwipeable({
+        onSwiped: (eventData) => {
+            console.log(progressionIndex)
+            if (eventData.dir == 'Left' && progressionIndex < progressions.current.length -1){
+                setProgressionIndex(progressionIndex + 1)
+            }
+            else if (eventData.dir == 'Right' && progressionIndex > 0) {
+                setProgressionIndex(progressionIndex - 1)
+            }
+        }
+    });
 
     useEffect(() => {
         const accordionChordsFilteredByDifficulty = ACCORDION_CHORDS.filter((x) => x.difficulty == +props.difficulty)[0];
-        const randomIndex = Math.floor(Math.random() * accordionChordsFilteredByDifficulty.progressions.length);
-        setChallenge(accordionChordsFilteredByDifficulty.progressions[randomIndex]);
+        progressions.current = shuffleArray(accordionChordsFilteredByDifficulty.progressions)
+
+        setProgression(progressions.current[progressionIndex]);
 
     }, []);
 
     useEffect(() => {
-        if (challenge != null) {
-            const timePerBeat = 60 / tempo * 1000;
-            const id = setInterval(() => {
-                tick(timePerBeat * 4)
-            }, timePerBeat);
+      
+        setProgression(progressions.current[progressionIndex]);
 
-            return () => clearInterval(id);
-        }
-    }, [challenge]);
+        indexRef.current = 0;
+
+        const timePerBeat = 60 / tempo * 1000;
+        const id = setInterval(() => {
+            tick(timePerBeat * 4)
+        }, timePerBeat);
+
+        return () => clearInterval(id);
+        
+    }, [progressionIndex]);
+
 
 
     useEffect(() => {
@@ -101,15 +134,15 @@ export default function challengePlayer(props: ChallengeProps) {
 
 
     return (
-        <div className="bars relative overflow-hidden h-96">
+        <div className="bars relative overflow-hidden h-96" {...handlers}>
             <p>Difficulty: {difficultyLevels[props.difficulty]}</p>
             <p>Instrument: {instruments[props.instrument]}</p>
+            
 
-
-            <p>{challenge?.name}</p>
-            <p>{challenge?.description}</p>
-            <p>{challenge?.chords}</p>
-            <p>{challenge?.tags.map((x, index) =>
+            <p>{progression?.name}</p>
+            <p>{progression?.description}</p>
+            <p>{progression?.chords}</p>
+            <p>{progression?.tags.map((x, index) =>
             (
                 <strong key={index}>{x} </strong>
             ))}
@@ -120,7 +153,7 @@ export default function challengePlayer(props: ChallengeProps) {
                   
                         <div className="bar-wrapper flex items-center">
                             <div ref={registerBars} className={`${styles['bar']} grid grid-cols-4 gap-1 w-full p-1`}>
-                              {challenge?.chords.map((chord, j) => (
+                              {progression?.chords.map((chord, j) => (
                                     <div key={j} ref={registerBeats} className={`${styles['beat']} rounded-sm bg-gray-500 p-5 text-center`} data-chord={chord}>
                                         {chord}
                                     </div>
