a31e68ed7eeaea6cb7d44fc982a114543b4fb7c0
Łukasz Zięba <kukens@wp.pl>
2026-04-07 16:02:44 +0200

    update manifest and meta

--- app/root.tsx
+++ app/root.tsx
@@ -37,14 +37,12 @@ export const links: Route.LinksFunction = () => [
 ];
 
 
-
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <Meta />
         <Links />
       </head>
