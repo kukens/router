d00790175aa7caffba6d953236a157d100e152ca
Łukasz Zięba <kukens@wp.pl>
2026-04-15 20:01:04 +0200

    manifest fullscreen

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,8 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "apple-touch-icon", href: "maskable_icon_x512.png" }
+  { rel: "apple-touch-icon", href: "/maskable_icon_x512.png" },
+  { rel: "manifest", href: "/manifest.webmanifest" }
 ];
 
 
